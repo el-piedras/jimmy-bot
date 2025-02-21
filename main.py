23888006328fb699fa05323072512292c90c415d
Stonesword <stonesword503@gmail.com>
2025-02-20 21:29:46 -0300

    Added /fetch-data
    Fetches relevant data from the DB and embeds it into a message

--- main.py
+++ main.py
@@ -89,4 +89,38 @@ async def AddUser(interaction: discord.Interaction, username: discord.Member):
     newEmbed.set_footer(text="212th Attack Battalion Tracker")
     await interaction.response.send_message(embed=newEmbed)
 
+
+@client.tree.command(name="fetch-data", description="Gets the specified user's join date, events attended and strikes", guild=GUILD_ID)
+async def GetUserInfo(interaction: discord.Interaction, user: discord.Member):
+
+    ## Checks if user is in database
+    userToFetch = user.nick or user.global_name or user.name
+    usernameCell = trooperSheet.find(userToFetch)
+
+    ## Cancel operation if NOT found
+    if not usernameCell:
+        await interaction.response.send_message(f"{interaction.user.mention} User was not found in the database.", ephemeral=True)
+        return
+    
+    ## Otherwise, get the values from the row
+    rowList = trooperSheet.get(f"B{usernameCell.row}:K{usernameCell.row}")
+    flatList = [
+        "Yes" if item == "TRUE" else "No" if item == "FALSE" else item for item in rowList[0]
+    ]
+
+    ## Generate response...
+    newEmbed = discord.Embed(title=f"Database data for {user.mention}", color=discord.Color.orange())
+    newEmbed.add_field(name="Username", value=flatList[0])
+    newEmbed.add_field(name="Rank", value=flatList[1])
+    newEmbed.add_field(name="Events Attended", value=flatList[2])
+    newEmbed.add_field(name="Weekly Events Attended", value=flatList[3])
+    newEmbed.add_field(name="Events Attended in Current Company", value=flatList[4])
+    newEmbed.add_field(name="Quota Strikes", value=flatList[5])
+    newEmbed.add_field(name="Inactivity Notice", value=flatList[6])   
+    newEmbed.add_field(name="Resets Taken this Month", value=flatList[7])   
+    newEmbed.add_field(name="Quota Complete?", value=flatList[8])
+    newEmbed.add_field(name="Join Date", value=flatList[9])
+    newEmbed.set_footer(text="212th Attack Battalion Tracker")
+    await interaction.response.send_message(embed=newEmbed, delete_after=60)
+
 client.run(credentials.botKey)
