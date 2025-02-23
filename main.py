d91f75d04ba27a82c134341118b2ca15ecf7f500
Stonesword <stonesword503@gmail.com>
2025-02-22 22:19:41 -0300

    Bugfix
    Fixed /quota-check not working properly
    NOTE: MUST CHANGE FORMAT LATER

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import gspread
 scope = ["https://www.googleapis.com/auth/spreadsheets"]
 gc = gspread.service_account_from_dict(credentials.sheetsKey)
 
-sheet_id = "1NhU9tel9pk23uAjVNpxBUUyodA7I-fWInC7TqP6I5AA"
+sheet_id = "1eIoGfj35pMok5UAR8VH6BmCVzKY9trFH9S1RVwqJesI"
 sheet = gc.open_by_key(sheet_id)
 
-trooperSheet = sheet.worksheet("Trooper")
+trooperSheet = sheet.worksheet("🚀TROOPER COMPANY")
 
 ## Discord Bot Init
 
@@ -48,25 +48,26 @@ async def AddUser(interaction: discord.Interaction, username: discord.Member):
 
     ## Checks if username exists in database
     userToAdd = username.nick or username.global_name or username.name
-    userExists = trooperSheet.find(userToAdd, in_column=2)
+    usernameCell = trooperSheet.find(userToAdd, in_column=2)
 
     ## If user is already in DB, send response and cancel
-    if userExists:
+    if usernameCell:
         await interaction.response.send_message(ephemeral=True, content=f"{interaction.user.mention} User {username.name} is already on the database.")
         return
-
-    ## Data to insert for new members
-    memberDefaultData = [userToAdd, "Recruit", 0, 0, 0, 0, "FALSE", 0, None, str(username.joined_at.date())]
 
     ## Flag to check if gaps exist  
     foundEmpty = False
 
-    ## Otherwise, gets usernames from the database
+    ## Gets usernames from the database
     trooperUsernameValue = trooperSheet.get_values("B19:B")
     trooperUsernameValue = [value[0] if value else "" for value in trooperUsernameValue]
 
     ## If gap is found, populate it first...
-    for i, value in enumerate(trooperUsernameValue, start=19):
+    for i, value in enumerate(trooperUsernameValue, start=20):
+
+        ## Data to insert for new members
+        memberDefaultData = [userToAdd, "Recruit", 0, 0, 0, 0, "FALSE", 0, f"=ARRAYFORMULA(IF(AND(E{i}>=3), TRUE, FALSE))", str(username.joined_at.date())]
+        
         if value == "":
             trooperSheet.update(range_name=f"B{i}:K{i}", values=[memberDefaultData], value_input_option="USER_ENTERED")
             foundEmpty = True
@@ -123,6 +124,7 @@ async def GetUserInfo(interaction: discord.Interaction, user: discord.Member):
     newEmbed.add_field(name="Resets Taken this Month", value=flatList[7])   
     newEmbed.add_field(name="Quota Complete?", value=flatList[8])
     newEmbed.add_field(name="Join Date", value=flatList[9])
+    newEmbed.add_field(name="Row in DB", value=usernameCell.row)
     newEmbed.set_footer(text="212th Attack Battalion Tracker")
     await interaction.response.send_message(embed=newEmbed, delete_after=60)
 
@@ -147,9 +149,9 @@ async def GetUserInfo(interaction: discord.Interaction, user: discord.Member):
         trooperSheet.delete_rows(usernameCell.row)
 
     else:
-        trooperSheet.update(range_name=f"B{usernameCell.row}:K{usernameCell.row}", values=[["","","0","0","0","0","FALSE","0","FALSE",""]], value_input_option="USER_ENTERED")
+        trooperSheet.update(range_name=f"B{usernameCell.row}:K{usernameCell.row}", values=[["","","0","0","0","0","FALSE","0",f"=ARRAYFORMULA(IF(AND(E{usernameCell.row}>=3), TRUE, FALSE))",""]], value_input_option="USER_ENTERED")
     
-    await interaction.response.send_message(ephemeral=True, content=f"{interaction.user.mention} User has been successfully removed from the database.")
+    await interaction.response.send_message(content=f"{interaction.user.mention} User has been successfully removed from the database.")
 
 @client.tree.command(name="quota-check", description="Checks if anyone in the database hasn't completed their quota and applies strike if necessary.", guild=GUILD_ID)
 async def CheckQuota(interaction: discord.Interaction):
@@ -179,14 +181,14 @@ async def CheckQuota(interaction: discord.Interaction):
         strikes = strikes[0] if strikes else "0"
         inactivity = inactivity[0] if inactivity else "FALSE"
         quotaPassed = quotaPassed[0] if quotaPassed else "FALSE"
-        date = str(date[0]) if date else str(datetime.today)
+        date = str(date[0]) if date else str(datetime.today().today)
         events = events[0] if events else "0"
 
         ## If there data missing, skip current iteration
         if not strikes or not inactivity or not date:
             continue
 
-        joinDateObj = datetime.strptime(date, "%Y-%m-%d") ## Convert join date cell into datetime object to compare it against seven days ago
+        joinDateObj = datetime.strptime(date, "%d/%m/%y") ## Convert join date cell into datetime object to compare it against seven days ago
 
         ## Logic for applying strikes
         if joinDateObj < sevenDaysAgo and inactivity == "FALSE" and quotaPassed == "FALSE":
